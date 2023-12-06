e12c032042b42d74b39a8cae4e047e6666453856
kavoom2 <kavoom2@gmail.com>
2023-12-07 00:13:06 +0900

    lesson 3 done

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,93 +1,249 @@
 ﻿using System;
+using System.Security.Cryptography;
 
-namespace MyCSharpProgram
+namespace CSharp
 {
-    class MyClass
+    class Program
     {
-        static int Add(int num1, int num2)
+        enum PlayerClassType
         {
-            return num1 + num2;
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
         }
 
-        static float Add(float num1, float num2)
+        enum MonsterType
         {
-            return num1 + num2;
+            None = 0,
+            Slime = 1,
+            Orc = 2,
+            Skeleton = 3
         }
 
-        static int Add(int num1, int num2, int num3 = 0, int num4 = 0)
+        struct Player
         {
-            return num1 + num2 + num3 + num4;
+            public int hp;
+            public int attack;
+            public PlayerClassType playerClass;
         }
 
-        // 메서드 오버로딩 -> 같은 이름의 메서드를 여러개 정의할 수 있습니다.
-        static void AddOne(int num)
+        struct Monster
         {
-            num += 1;
-            Console.WriteLine(num);
+            public int hp;
+            public int attack;
+            public MonsterType monsterType;
         }
 
-        static void AddOneByRef(ref int num) // reference -> 값이 아닌 주소를 전달합니다.
+        static PlayerClassType SelectPlayerClass()
         {
-            num += 1;
-            Console.WriteLine(num);
-        }
+            PlayerClassType selectedPlayerClass = PlayerClassType.None;
 
-        static int Sum(int[] numbers)
-        {
-            int sum = 0;
+            Console.WriteLine("직업을 선택해 주세요.");
+            Console.WriteLine("[1] 전사");
+            Console.WriteLine("[2] 궁수");
+            Console.WriteLine("[3] 법사");
 
-            foreach (int num in numbers)
+            string input = Console.ReadLine() ?? "";
+
+            switch (input)
             {
-                sum += num;
+                case "1":
+                {
+                    selectedPlayerClass = PlayerClassType.Knight;
+                    break;
+                }
+                case "2":
+                {
+                    selectedPlayerClass = PlayerClassType.Archer;
+                    break;
+                }
+                case "3":
+                {
+                    selectedPlayerClass = PlayerClassType.Mage;
+                    break;
+                }
             }
 
-            return sum;
+            return selectedPlayerClass;
         }
 
-        static void Swap(ref int num1, ref int num2)
+        static void CreatePlayer(PlayerClassType playerClass, out Player player)
         {
-            (num1, num2) = (num2, num1);
+            switch (playerClass)
+            {
+                case PlayerClassType.Knight:
+                {
+                    player.hp = 100;
+                    player.attack = 10;
+                    player.playerClass = PlayerClassType.Knight;
+                    break;
+                }
+                case PlayerClassType.Archer:
+                {
+                    player.hp = 75;
+                    player.attack = 12;
+                    player.playerClass = PlayerClassType.Archer;
+                    break;
+                }
+                case PlayerClassType.Mage:
+                {
+                    player.hp = 50;
+                    player.attack = 15;
+                    player.playerClass = PlayerClassType.Mage;
+                    break;
+                }
+                default:
+                {
+                    player.hp = 0;
+                    player.attack = 0;
+                    player.playerClass = PlayerClassType.None;
+                    break;
+                }
+            }
         }
 
-        static void Divide(int num1, int num2, out int result, out int remainder) // out -> 값을 반환하지 않고, 참조로 전달합니다.
+        static void CreateRandomMonster(out Monster monster)
         {
-            result = num1 / num2;
-            remainder = num1 % num2;
+            Random rand = new();
+            int randMonster = rand.Next(1, 4);
+
+            switch (randMonster)
+            {
+                case (int)MonsterType.Slime:
+                {
+                    monster.hp = 20;
+                    monster.attack = 2;
+                    monster.monsterType = MonsterType.Slime;
+                    break;
+                }
+                case (int)MonsterType.Orc:
+                {
+                    monster.hp = 40;
+                    monster.attack = 4;
+                    monster.monsterType = MonsterType.Orc;
+                    break;
+                }
+                case (int)MonsterType.Skeleton:
+                {
+                    monster.hp = 30;
+                    monster.attack = 3;
+                    monster.monsterType = MonsterType.Skeleton;
+                    break;
+                }
+                default:
+                {
+                    monster.hp = 0;
+                    monster.attack = 0;
+                    monster.monsterType = MonsterType.None;
+                    break;
+                }
+            }
         }
 
-        static void MyMethod()
+        static void EnterGame(ref Player player)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("게임에 접속했습니다.");
+                Console.WriteLine("[1] 필드로 간다.");
+                Console.WriteLine("[2] 로비로 돌아간다.");
+
+                string input = Console.ReadLine() ?? "";
+
+                if (input == "1")
+                {
+                    EnterField(ref player);
+                }
+                else if (input == "2")
+                {
+                    break;
+                }
+            }
+        }
+
+        static void EnterField(ref Player player)
+        {
+            while (true)
+            {
+                Console.WriteLine("필드에 접속했습니다.");
+
+                CreateRandomMonster(out Monster monster);
+                Console.WriteLine($"몬스터가 출현했습니다. ({monster.monsterType})");
+
+                Console.WriteLine("[1] 전투 모드로 돌입한다.");
+                Console.WriteLine("[2] 일정 확률로 도망친다.");
+
+                string input = Console.ReadLine() ?? "";
+                if (input == "1")
+                {
+                    EnterBattle(ref player, ref monster);
+                }
+                else if (input == "2")
+                {
+                    TryEscape(ref player, ref monster);
+                }
+
+                if (player.hp <= 0)
+                {
+                    Console.WriteLine("사망했습니다.");
+                    break;
+                }
+            }
+        }
+
+        static void EnterBattle(ref Player player, ref Monster monster)
+        {
+            while (true)
+            {
+                monster.hp -= player.attack;
+                if (monster.hp <= 0)
+                {
+                    Console.WriteLine("승리했습니다.");
+                    Console.WriteLine($"남은 체력: {player.hp}");
+                    break;
+                }
+
+                player.hp -= monster.attack;
+                if (player.hp <= 0)
+                {
+                    Console.WriteLine("패배했습니다.");
+                    break;
+                }
+            }
+        }
+
+        static void TryEscape(ref Player player, ref Monster monster)
+        {
+            Random rand = new();
+            int randValue = rand.Next(0, 101);
+
+            if (randValue < 33)
+            {
+                Console.WriteLine("도망치는데 성공했습니다.");
+            }
+            else
+            {
+                Console.WriteLine("도망치는데 실패했습니다.");
+                EnterBattle(ref player, ref monster);
+            }
         }
 
         static void Main(string[] args)
         {
-            int myNum = 1;
+            while (true)
+            {
+                PlayerClassType selectedPlayerClass = SelectPlayerClass();
 
-            AddOne(myNum);
-            Console.WriteLine(myNum);
+                if (selectedPlayerClass != PlayerClassType.None)
+                {
+                    CreatePlayer(selectedPlayerClass, out Player player);
 
-            AddOneByRef(ref myNum);
-            Console.WriteLine(myNum);
-
-            int[] numbers = { 1, 2, 3, 4, 5 };
-
-            Swap(ref numbers[0], ref numbers[1]); // reference -> 값이 아닌 주소를 전달합니다.
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers);
-
-            int result;
-            int remainder;
-
-            Divide(10, 3, out result, out remainder); // out -> 값을 반환하지 않고, 참조로 전달합니다.
-            Console.WriteLine(result);
-            Console.WriteLine(remainder);
-
-            Console.WriteLine(Add(1.2f, 1.0f));
-            Console.WriteLine(Add(1, 2));
-            Console.WriteLine(Add(1, 2, 3)); // 선택적 매개변수
-            Console.WriteLine(Add(1, 2, num4: 4)); // 키워드 매개변수
+                    Console.WriteLine("선택한 직업: " + player.playerClass);
+                    Console.WriteLine($"HP: {player.hp}, 공격력: {player.attack}");
+                    EnterGame(ref player);
+                }
+            }
         }
     }
 }
